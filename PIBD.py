d5f227ecfa1ef1d47ba7149e9ca3a5e0317bf99b
Victor Hugo Rodrigues <91756633+VictorRodrigues-03@users.noreply.github.com>
2025-07-17 14:44:12 -0300

    Versão final
    Lembrando que para o funcionamento correto as 2 Functions, 4 Procedures e 2 Views utilizados devem estar criadas no Banco

--- PIBD.py
+++ PIBD.py
@@ -1,19 +1,117 @@
 import psycopg2
 from psycopg2 import Error
 
+DB_HOST     = '---------'
+DB_NAME     = '---------'
+DB_USER     = '---------'
+DB_PASSWORD = '---------'
+DB_PORT     = '---------'
+
+STATUS = ['Aberta','Em Andamento','Concluída','Cancelada']
+
 def get_db_connection():
     try:
-        conn = psycopg2.connect(
-            host="localhost",
-            database="PIDB_BASE",
-            user="postgres",
-            password="arthur12*",
-            port="5432"
-        )
-        return conn
+            conn = psycopg2.connect(
+                host     = DB_HOST,
+                database = DB_NAME,
+                user     = DB_USER,
+                password = DB_PASSWORD,
+                port     = DB_PORT
+            )
+            return conn
     except Exception as error:
         print(f"Erro ao conectar ao banco de dados: {error}")
         return None
+    
+    
+ ##Funções para checar existencia no Banco   
+    
+def local_E(cep, nmr):
+    conn = get_db_connection()
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT 1 FROM Local WHERE cep = %s AND numero = %s",
+                    [cep,nmr]
+                )
+                return cursor.fetchone() is not None
+        except Error as e:
+            print(f"Error: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
+            
+def tec_E(cpf):
+    conn = get_db_connection()
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT 1 FROM Tecnico WHERE cpf = %s",
+                    [cpf]
+                )
+                return cursor.fetchone() is not None
+        except Error as e:
+            print(f"Error: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
+            
+def ocorr_E(nmr_ocorrencia):
+    conn = get_db_connection()
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT 1 FROM Ocorrencia WHERE id_ocorrencia = %s",
+                    [nmr_ocorrencia]
+                )
+                return cursor.fetchone() is not None
+        except Error as e:
+            print(f"Error: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
+
+def user_E(cpf):
+    conn = get_db_connection()
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT 1 FROM Usuario WHERE cpf = %s",
+                    [cpf]
+                )
+                return cursor.fetchone() is not None
+        except Error as e:
+            print(f"Error: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
+            
+##Funções de inserção no banco
+
+def criar_local(cep,numero):
+    print("Local não existe no banco, forneça os seguintes dados")
+    bairro = input("Bairro: ")
+    logradouro = input("Logradouro: ")
+    complemento = input("Complemento: ")
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                "CALL criar_local(%s, %s, %s, %s, %s)",
+                [cep, numero, complemento, bairro, logradouro]
+            )
+            conn.commit()
+        print("Local Criado com Sucesso!!")
+    except Exception as e:
+        print(f"Erro ao criar Local: {e}")
+        conn.rollback()
+    finally:
+        conn.close()
+
 
 def criar_usuario(cpf, nome, fone, email):
     conn = get_db_connection()
@@ -21,44 +119,30 @@ def criar_usuario(cpf, nome, fone, email):
         try:
             with conn.cursor() as cursor:
                 cursor.execute(
-                    "INSERT INTO usuario (cpf, nome, fone, email) VALUES (%s, %s, %s, %s)",
-                    (cpf, nome, fone, email)
+                    "CALL criar_usuario(%s, %s, %s, %s)",
+                    [cpf, nome, fone, email]
                 )
                 conn.commit()
                 print("Usuário criado com sucesso!")
         except Exception as e:
-            if hasattr(e, 'pgcode') and e.pgcode == '23505':  # Código de erro conflito de chave primary
-                print(f"Erro: CPF, telefone ou e-mail já cadastrado para {nome}.")
-            else:
-                print(f"Erro ao criar usuário: {e}")
+            print(f"Erro ao criar usuário: {e}")
             conn.rollback()
         finally:
             conn.close()
+                
 
 def criar_ocorrencia(cpf_solicitante, cep, numero):
     conn = get_db_connection()
     if conn:
         try:
             with conn.cursor() as cursor:
-                cursor.execute("CALL sp_criar_ocorrencia(%s, %s, %s, NULL, NULL)",
-                            (cpf_solicitante, cep, numero)
+                if(not local_E(cep,numero)):
+                    criar_local(cep,numero)
+                cursor.execute("CALL criar_ocorrencia(%s, %s, %s)",
+                            [cpf_solicitante, cep, numero]
                 )
                 conn.commit()
                 print("Ocorrência criada com sucesso!")
-                # Recupera os IDs da ocorrência e do solicitante
-                cursor.execute("""
-                    SELECT id_ocorrencia, id_solicitante
-                    FROM Ocorrencia
-                    WHERE id_solicitante = (SELECT id_solicitante FROM Solicitante WHERE cpf = %s)
-                    ORDER BY id_ocorrencia DESC
-                    LIMIT 1;
-                """, (cpf_solicitante,))
-                result = cursor.fetchone()
-                if result:
-                    ocorrencia_id, solicitante_id = result
-                    print(f"Ocorrência {ocorrencia_id} criada para o solicitante {solicitante_id} (CPF: {cpf_solicitante}).")
-                else:
-                    print(f"Ocorrência criada, mas não foi possível recuperar os IDs.")
             conn.commit()
         except Error as e:
             print(f"Erro ao criar ocorrência: {e}")
@@ -66,67 +150,121 @@ def criar_ocorrencia(cpf_solicitante, cep, numero):
         finally:
             conn.close()
 
-def definir_tecnico_ocorrencia(id_ocorrencia, id_tecnico):
+def criar_tecnico(cpf):
     conn = get_db_connection()
     if conn:
         try:
             with conn.cursor() as cursor:
-                cursor.execute(
-                    "UPDATE ocorrencia SET id_tecnico = %s WHERE id_ocorrencia = %s",
-                    (id_tecnico, id_ocorrencia)
+                if(not user_E(cpf)):
+                    print("Necessario criar o usuario antes")
+                    nome = input("Nome: ")
+                    fone = input("Telefone: ")
+                    email = input("E-mail: ")
+                    criar_usuario(cpf, nome, fone, email)
+                cursor.execute("CALL criar_tecnico(%s)",
+                            [cpf]
                 )
-                if cursor.rowcount > 0:
-                    print(f"Técnico {id_tecnico} atribuído à ocorrência {id_ocorrencia}.")
-                else:
-                    print(f"Nenhuma ocorrência encontrada com o ID {id_ocorrencia}.")
-            conn.commit()
+                print("Tecnico criado com sucesso")
+                conn.commit()
         except Error as e:
-            print(f"Erro ao atribuir técnico à ocorrência: {e}")
+            print(f"Erro ao criar Tecnico: {e}")
             conn.rollback()
         finally:
             conn.close()
+            
+            
+##Funções de interface entre o programa e o banco          
 
-def atualizar_status_ocorrencia(id_ocorrencia,new_status):
+def ocorr_D():
     conn = get_db_connection()
     if conn:
         try:
-            with conn.cursor() as cursor:
-                cursor.execute(
-                    "UPDATE ocorrencia SET status = %s WHERE id_ocorrencia = %s",
-                    (new_status, id_ocorrencia)
-                )
-                if cursor.rowcount > 0:
-                    print(f"Status da ocorrência {id_ocorrencia} atualizado para '{new_status}'.")
-                else:
-                    print(f"Nenhuma ocorrência encontrada com o ID {id_ocorrencia}.")
+            with conn.cursor as cursor:
+                cursor.execute("Select * FROM Ocorrencia WHERE status = %s",STATUS[0])
+                tabela = cursor.fetchall()
+                for i in tabela:
+                    print(i)
             conn.commit()
         except Error as e:
-            print(f"Erro ao atualizar status da ocorrência: {e}")
+            print(f"Erro ao criar Tecnico: {e}")
             conn.rollback()
         finally:
             conn.close()
-def ocorrencias_por_tecnico(id_tecnico):
+ 
+def ocorrencias_abertas():
     conn = get_db_connection()
     if conn:
         try:
             with conn.cursor() as cursor:
                 cursor.execute(
-                    "SELECT * FROM fn_get_ocorrencias_por_tecnico(%s)", (id_tecnico,)
-                    )
+                    "SELECT * FROM Ocorrencia WHERE status = %s OR status = %s", [STATUS[0],STATUS[1]])
                 ocorrencias = cursor.fetchall()
                 if ocorrencias:
-                    print(f"Ocorrências atribuídas ao técnico {id_tecnico}:")
+                    print(f"Ocorrências Disponíveis")
                     for ocorrencia in ocorrencias:
                         print(ocorrencia)
                 else:
-                    print(f"Nenhuma ocorrência encontrada para o técnico {id_tecnico}.")
+                    print("Sem Ocorrências disponíveis")
+                    return False
             conn.commit()
+            return True
+        except Error as e:
+            print(f"Erro ao buscar ocorrências do técnico: {e}")
+            conn.rollback()
+        finally:
+            conn.close() 
+            
+def ocorrencias_por_tecnico(cpf_tecnico):
+    conn = get_db_connection()
+    if conn:
+        try:
+            if(not tec_E(cpf_tecnico)):
+                print("Tecnico não existe")
+                return False
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT * FROM fn_get_ocorrencias_por_tecnico(%s)", [cpf_tecnico])
+                ocorrencias = cursor.fetchall()
+                if ocorrencias:
+                    print(f"Ocorrências atribuídas ao técnico {cpf_tecnico}:")
+                    for ocorrencia in ocorrencias:
+                        print(ocorrencia)
+                else:
+                    print(f"Nenhuma ocorrência encontrada para o técnico {cpf_tecnico}.")
+                    return False
+            conn.commit()
+            return True
         except Error as e:
             print(f"Erro ao buscar ocorrências do técnico: {e}")
             conn.rollback()
         finally:
             conn.close()
-
+            
+def ocorrencias_por_user(cpf_tecnico):
+    conn = get_db_connection()
+    if conn:
+        try:
+            if(not user_E(cpf_tecnico)):
+                print("Usuario não existe")
+                return False
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT * FROM vw_ocorrencias_por_usuario WHERE cpf = %s", [cpf_tecnico])
+                ocorrencias = cursor.fetchall()
+                if ocorrencias:
+                    print(f"Ocorrências atribuídas ao Usuario: {cpf_tecnico}:")
+                    for ocorrencia in ocorrencias:
+                        print(ocorrencia)
+                else:
+                    print(f"Nenhuma ocorrência encontrada para o Usuario {cpf_tecnico}.")
+            conn.commit()
+            return True
+        except Error as e:
+            print(f"Erro ao buscar ocorrências do Usuario: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
+            
 def listar_tabelas(tabel_name):
     conn = get_db_connection()
     if conn:
@@ -150,17 +288,87 @@ def listar_tabelas(tabel_name):
             print(f"Erro ao listar dados da tabela '{tabel_name}': {e}")
         finally:
             conn.close()
+            
+            
+##Funções de edição
+
+def definir_tecnico_ocorrencia(id_ocorrencia, cpf_tecnico):
+    conn = get_db_connection()
+    if conn:
+        try:
+            if(not ocorr_E(id_ocorrencia)):
+                print("Ocorrência não existe")
+                return
+            elif(not tec_E(cpf_tecnico)):
+                print("Tecnico não existe")
+                return
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT atribuir_ocorrencia(%s,%s::smallint)",
+                    [cpf_tecnico, id_ocorrencia]
+                )
+                if cursor.rowcount > 0:
+                    print(f"Técnico {cpf_tecnico} atribuído à ocorrência {id_ocorrencia}.")
+            conn.commit()
+        except Error as e:
+            print(f"Erro ao atribuir técnico à ocorrência: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
+
+def atualizar_status_ocorrencia(id_ocorrencia,new_status):
+    conn = get_db_connection()
+    if conn:
+        try:
+            if(STATUS.count(new_status) == 0):
+                print("Status inválido.")
+                return
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "UPDATE ocorrencia SET status = %s WHERE id_ocorrencia = %s",
+                    [new_status, id_ocorrencia]
+                )
+                if cursor.rowcount > 0:
+                    print(f"Status da ocorrência {id_ocorrencia} atualizado para '{new_status}'.")
+            conn.commit()
+        except Error as e:
+            print(f"Erro ao atualizar status da ocorrência: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
+            
+def conclui_ocorrencia(id_ocorrencia):
+    conn = get_db_connection()
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    "UPDATE ocorrencia SET status = %s WHERE id_ocorrencia = %s",
+                    [STATUS[2], id_ocorrencia]
+                )
+                if cursor.rowcount > 0:
+                    print(f"Status da ocorrência {id_ocorrencia} atualizado para 'Concluida'.")
+            conn.commit()
+        except Error as e:
+            print(f"Erro ao atualizar status da ocorrência: {e}")
+            conn.rollback()
+        finally:
+            conn.close()
+
 
 def main():
     while True:
         print("\nMenu:")
         print("1. Criar Usuário")
         print("2. Criar Ocorrência")
-        print("3. Atualizar Status da Ocorrência")
-        print("4. Listar Ocorrências por Técnico")
-        print("5. Vincular Técnico a Ocorrência")
-        print("6. Listar Tabelas")
-        print("7. Sair")
+        print("3. Criar Técnico")
+        print("4. Atribuir uma Ocorrência a um técnico")
+        print("5. Concluir uma Ocorrência")
+        print("6. Listar Ocorrências de um Técnico")
+        print("7. Listar Ocorrências de um Usuário")
+        print("8. Sair")
+        
+        ##Parei aqui, o trigger será sempre que um técnico for adicionado, devemos adicionar uma ocorrencia a ele.
 
         choice = input("Escolha uma opção: ")
 
@@ -176,20 +384,34 @@ def main():
             numero = input("Número da Casa: ")
             criar_ocorrencia(cpf_solicitante, cep, numero)
         elif choice == '3':
-            id_ocorrencia = int(input("ID da Ocorrência: "))
-            new_status = input("Novo Status: ")
-            atualizar_status_ocorrencia(id_ocorrencia, new_status)
+            cpf_tecnico = input("CPF do Ténico: ")
+            criar_tecnico(cpf_tecnico)
         elif choice == '4':
-            id_tecnico = int(input("ID do Técnico: "))
-            ocorrencias_por_tecnico(id_tecnico)
+            cpf_tecnico = input("CPF do Técnico: ")
+            if(tec_E(cpf_tecnico)):
+                ocorrencias_abertas()
+                id_ocorrencia = int(input("Digite o ID da ocorrencia: "))
+                definir_tecnico_ocorrencia(id_ocorrencia,cpf_tecnico)
+            else:
+                print("Tecnico não existe.")
         elif choice == '5':
-            id_ocorrencia = int(input("ID da Ocorrência: "))
-            id_tecnico = int(input("ID do Técnico: "))
-            definir_tecnico_ocorrencia(id_ocorrencia, id_tecnico)
+            cpf_tecnico = input("CPF do Técnico: ")
+            if(tec_E(cpf_tecnico)):
+                ocorrencias_por_tecnico(cpf_tecnico)
+                id_ocorrencia = int(input("Digite o ID da ocorrencia a ser concluída: "))
+                conclui_ocorrencia(id_ocorrencia)
+            else:
+                print("Tecnico não existe.")
         elif choice == '6':
-            table_name = input("Nome da Tabela: ")
-            listar_tabelas(table_name)
+            cpf_tecnico = input("CPF do Técnico: ")
+            if(tec_E(cpf_tecnico)):
+                ocorrencias_por_tecnico(cpf_tecnico)
+            else:
+                print("Tecnico não existe.")   
         elif choice == '7':
+            cpf_usr = input("CPF do usuario: ")
+            ocorrencias_por_user(cpf_usr)
+        elif choice == '8':
             break
         else:
             print("Opção inválida. Tente novamente.")
